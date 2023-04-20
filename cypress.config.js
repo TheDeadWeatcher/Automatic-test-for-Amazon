a3c93a99c7e1528ab3f40afd8876a5c99de56b63
Bartosz <jasykbartosz@gmail.com>
2023-04-20 17:41:17 +0200

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      
+    },
+
+    baseUrl: "https://www.amazon.com/",
+    includeShadowDom: true,
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    video: false,
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
+  },
+});
---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+describe('E2E - home page - Amazon', () => {
+
+    before(function(){
+        cy.fixture('pagesUrl').then(function(url){
+            globalThis.url = url;
+        });
+    });
+
+
+    beforeEach(() => {
+        cy.visit('/');
+    });
+
+    it('Should open home page and verify url', () => {
+        cy.url().should('eq', url.homeUrl);
+    });
+});
