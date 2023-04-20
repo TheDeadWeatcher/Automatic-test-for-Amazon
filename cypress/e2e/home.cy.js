0bb119f6ac901d73fbb3f63eaf9ea49193a51adc
Bartosz <jasykbartosz@gmail.com>
2023-04-20 20:56:35 +0200

    20.04 end session

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -16,18 +16,20 @@ describe("E2E - home page - Amazon", () => {
   });
 
   it("should search for a valid product", () => {
-    const searchTerm = "iphone"; // define the search term
-    cy.get("#twotabsearchtextbox").type(searchTerm); // find the search input and type in the search term
-    cy.get(".nav-search-submit .nav-input").click(); // click the search button
-    cy.url().should("include", `k=${searchTerm}`); // check that the URL contains the search term
-    cy.get(".s-result-list").should("have.length.gte", 1); // check that there is at least one search result
+    const searchTerm = "iphone"; 
+    cy.get("#twotabsearchtextbox").type(searchTerm); 
+    cy.get(".nav-search-submit .nav-input").click(); 
+    cy.url().should("include", `k=${searchTerm}`);
+    cy.get(".s-result-list").should("have.length.gte", 1); 
   });
 
-  it("should show no results for an invalid term", () => {
-    const searchTerm = "qwertyuiop";
-    cy.get("#twotabsearchtextbox").type(searchTerm); // find the search input and type in the search term
-    cy.get(".nav-search-submit .nav-input").click(); // click the search button
-    cy.url().should("include", `k=${searchTerm}`); // check that the URL contains the search term
-    cy.get("#noResultsTitle").should("be.visible"); // check that the "no results" message is displayed
+  it.only("should show no results for an invalid term", () => {
+    const searchTerm = "audhsuadh";
+    cy.get("#twotabsearchtextbox").type(searchTerm); 
+    cy.get(".nav-search-submit .nav-input").click(); 
+    cy.url().should("include", `k=${searchTerm}`); 
+    cy.get("#noResultsTitle").should("be.visible"); 
   });
+
+  
 });
