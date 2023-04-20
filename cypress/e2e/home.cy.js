6dee2564039ea01393ec4692d958bf25934400b2
Bartosz <jasykbartosz@gmail.com>
2023-04-20 18:05:38 +0200

    home page work

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -14,4 +14,24 @@ describe("E2E - home page - Amazon", () => {
   it("Should open home page and verify url", () => {
     cy.url().should("eq", url.homeUrl);
   });
+
+  // it('Should type phrase and verify title', () => {
+    
+  // });
+
+  it("should search for a valid product", () => {
+    const searchTerm = "iphone"; // define the search term
+    cy.get("#twotabsearchtextbox").type(searchTerm); // find the search input and type in the search term
+    cy.get(".nav-search-submit .nav-input").click(); // click the search button
+    cy.url().should("include", `k=${searchTerm}`); // check that the URL contains the search term
+    cy.get(".s-result-list").should("have.length.gte", 1); // check that there is at least one search result
+  });
+
+  it("should show no results for an invalid term", () => {
+    const searchTerm = "qwertyuiop";
+    cy.get("#twotabsearchtextbox").type(searchTerm); // find the search input and type in the search term
+    cy.get(".nav-search-submit .nav-input").click(); // click the search button
+    cy.url().should("include", `k=${searchTerm}`); // check that the URL contains the search term
+    cy.get("#noResultsTitle").should("be.visible"); // check that the "no results" message is displayed
+  });
 });
